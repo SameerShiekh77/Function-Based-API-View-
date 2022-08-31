327a1ddfc7c24a8e34b7a9535b84f1c0b60b5a80
Muhammad Sameer <muhammadsameer.css@gmail.com>
2022-08-31 12:22:40 +0500

    create class api view

--- api/urls.py
+++ api/urls.py
@@ -2,6 +2,13 @@
 from django.urls import path
 from . import views 
 urlpatterns = [
+
+    # FUNCTION BASE VIEW
     path('student-list/',views.studentApi,name='index'),
     path('student-list/<int:pk>/',views.studentApi,name='student-details'),
+
+
+# CLASS BASE VIEW
+    path('class-student-list/',views.StudentAPI.as_view(),name='student-details'),
+    path('class-student-list/<int:pk>/',views.StudentAPI.as_view(),name='student-details'),
 ]
--- api/views.py
+++ api/views.py
@@ -3,11 +3,15 @@ from rest_framework.response import Response
 from .serializer import StudentSerializer
 from .models import Student
 from rest_framework import status
+from rest_framework.views import APIView
 
 # Create your views here.
 
-@api_view(['GET','POST','PUT','PATCH','DELETE'])
-def studentApi(request,pk=None):
+
+# FUNCTION API VIEW
+
+@api_view(['GET', 'POST', 'PUT', 'PATCH', 'DELETE'])
+def studentApi(request, pk=None):
 
     # GET ALL DATA FROM DATABASE
     if request.method == 'GET':
@@ -18,16 +22,17 @@ def studentApi(request,pk=None):
             return Response(serializer.data)
 
         stu = Student.objects.all()
-        serializer = StudentSerializer(stu,many=True)
+        serializer = StudentSerializer(stu, many=True)
         return Response(serializer.data)
 
 
 # POST DATA IN DATABASE
     elif request.method == 'POST':
-        serializer = StudentSerializer(data=request.data,status=status.HTTP_201_CREATED)
+        serializer = StudentSerializer(
+            data=request.data, status=status.HTTP_201_CREATED)
         if serializer.is_valid():
             serializer.save()
-            return Response({"msg":"data created","data":serializer.data})
+            return Response({"msg": "data created", "data": serializer.data})
         return Response(serializer.errors)
 
 
@@ -36,24 +41,26 @@ def studentApi(request,pk=None):
         id = pk
         if id is not None:
             stu = Student.objects.get(pk=id)
-            serializer = StudentSerializer(stu,data=request.data,status=status.HTTP_205_RESET_CONTENT)
+            serializer = StudentSerializer(
+                stu, data=request.data, status=status.HTTP_205_RESET_CONTENT)
             if serializer.is_valid():
                 serializer.save()
-                return Response({"msg":"data complete updated","data":serializer.data})
+                return Response({"msg": "data complete updated", "data": serializer.data})
             return Response(serializer.errors)
-    
+
 # FOR PARTIALLY UPDATE DATA YOU NEED TO USE METHOD PATCH
-    
+
     elif request.method == 'PATCH':
         id = pk
         if id is not None:
             stu = Student.objects.get(pk=id)
-            serializer = StudentSerializer(stu,data=request.data,partial=True,status=status.HTTP_206_PARTIAL_CONTENT)
+            serializer = StudentSerializer(
+                stu, data=request.data, partial=True, status=status.HTTP_206_PARTIAL_CONTENT)
             if serializer.is_valid():
                 serializer.save()
-                return Response({"msg":"data partially updated","data":serializer.data})
+                return Response({"msg": "data partially updated", "data": serializer.data})
             return Response(serializer.errors)
-    
+
 
 # FOR DELETE
 
@@ -61,5 +68,56 @@ def studentApi(request,pk=None):
         id = pk
         stu = Student.objects.get(pk=id)
         stu.delete()
-        return Response({"msg":"data deleted"})
-        
+        return Response({"msg": "data deleted"})
+
+
+
+# CLASS API VIEW
+class StudentAPI(APIView):
+    def get(self, request, pk=None, format=None):
+        id = pk
+        if id is not None:
+            stu = Student.objects.get(id=id)
+            serializer = StudentSerializer(stu)
+            return Response(serializer.data)
+
+        stu = Student.objects.all()
+        serializer = StudentSerializer(stu,many=True)
+        return Response(serializer.data)
+    
+
+    def post(self,request,format=None):
+        serializer = StudentSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"msg": "data created", "data": serializer.data},status=status.HTTP_201_CREATED)
+        return Response(serializer.errors)
+
+
+    def put(self,request,pk,format=None):
+        id = pk
+        if id is not None:
+            stu = Student.objects.get(pk=id)
+            serializer = StudentSerializer(
+                stu, data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response({"msg": "data complete updated", "data": serializer.data}, status=status.HTTP_205_RESET_CONTENT)
+            return Response(serializer.errors)
+
+    def patch(self,request,pk,format=None):
+        id = pk
+        if id is not None:
+            stu = Student.objects.get(pk=id)
+            serializer = StudentSerializer(
+                stu, data=request.data, partial=True)
+            if serializer.is_valid():
+                serializer.save()
+                return Response({"msg": "data partially updated", "data": serializer.data}, status=status.HTTP_206_PARTIAL_CONTENT)
+            return Response(serializer.errors)
+
+    def delete(self,request,pk,format=None):
+        id = pk
+        stu = Student.objects.get(pk=id)
+        stu.delete()
+        return Response({"msg": "data deleted"})
